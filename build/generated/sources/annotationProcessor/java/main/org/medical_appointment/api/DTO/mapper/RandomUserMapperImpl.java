3515a60cf530d4e2ee041a7a4a3919deb82d8a26
konradoCh <chochorowski.konrad@gmail.com>
2024-02-19 19:40:10 +0100

    build folder commit

--- build/generated/sources/annotationProcessor/java/main/org/medical_appointment/api/DTO/mapper/RandomUserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/org/medical_appointment/api/DTO/mapper/RandomUserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-18T14:28:31+0100",
+    date = "2024-02-19T19:37:41+0100",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.0.jar, environment: Java 18.0.1.1 (Oracle Corporation)"
 )
 @Component
